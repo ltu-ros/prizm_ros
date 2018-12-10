7aa7f47a46297dc99f3eca55d4c9e43fccd59ab7
Nick Paul <npaul@ltu.edu>
2018-12-10 14:29:34 -0500

    add IR read

--- arduino/PRIZM/PRIZM_MC/PRIZM_MC.ino
+++ arduino/PRIZM/PRIZM_MC/PRIZM_MC.ino
@@ -1,8 +1,8 @@
 // ROS
 #include <ros.h>
-#include <std_msgs/Empty.h>
 #include <std_msgs/Bool.h>
 #include <geometry_msgs/Point.h>
+#include <std_msgs/UInt32.h>
 
 // Tetrix Prizm
 #include <PRIZM.h>
@@ -11,6 +11,8 @@ PRIZM prizm;
 // Direction
 #define FWD 1
 #define BKWD 0
+
+#define IR_PIN 15
 
 // Callback function for the message
 void motorCB( const geometry_msgs::Point& point)
@@ -28,10 +30,13 @@ void greenLEDCB( const std_msgs::Bool& toggle_msg)
     prizm.setGreenLED(toggle_msg.data ? HIGH : LOW);
 }
 
+std_msgs::UInt32 ir_msg;
+
 ros::NodeHandle nh;
 ros::Subscriber<geometry_msgs::Point> mc_sub("motor_cmd", &motorCB);
 ros::Subscriber<std_msgs::Bool>       red_led_sub("red_led",   &redLEDCB);
 ros::Subscriber<std_msgs::Bool>       green_led_sub("green_led", &greenLEDCB);
+ros::Publisher      ir_pub("ir_dist", &ir_msg);
 
 void setup()
 {
@@ -39,16 +44,23 @@ void setup()
     nh.subscribe(mc_sub);
     nh.subscribe(red_led_sub);
     nh.subscribe(green_led_sub);
+    nh.advertise(ir_pub);
+
 
     prizm.PrizmBegin();
     prizm.setRedLED(LOW);
     prizm.setGreenLED(LOW);
     prizm.setMotorInvert(2, 1); // Invert right motor
 
+    pinMode(IR_PIN, INPUT);
 }
+
 
 void loop()
 {
+    ir_msg.data = analogRead(IR_PIN);
+    ir_pub.publish(&ir_msg);
+
     nh.spinOnce();
     delay(1);
 }
